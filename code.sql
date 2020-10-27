5d19240fb5ce275883a5e68c6d3bc1d41d8500eb
Yash Chaturvedi <32377724+YashChat@users.noreply.github.com>
2020-10-26 21:08:53 -0400

    Update code.sql

--- code.sql
+++ code.sql
@@ -26,3 +26,17 @@ CREATE TABLE Vehicle_Brand
         Make_id VARCHAR(5) NOT NULL,
         FOREIGN KEY(Make_id) REFERENCES Make(Make_id)
  );
+
+CREATE TABLE Make
+    (
+        Make_id VARCHAR(5) NOT NULL,
+        Model_Shape VARCHAR(3) NOT NULL,
+        Engine_Power VARCHAR(20) NOT NULL,
+        0-60mph NUMBER(6) NOT NULL,
+        Price number(7) NOT NULL,
+        PRIMARY KEY (Make_id),
+        Color_id VARCHAR(3) NOT NULL,
+        FOREIGN KEY(Cart_id) REFERENCES Cart(Cart_id)
+        Accessories_id VARCHAR(3) NOT NULL,
+        FOREIGN KEY(Accessories_id) REFERENCES Accessories(Accessories_id)
+    ); 
