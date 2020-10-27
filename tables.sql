31e9c28591b99ed0649310c2804629f626023645
Yash Chaturvedi <32377724+YashChat@users.noreply.github.com>
2020-10-26 21:24:24 -0400

    Update and rename code.sql to tables.sql

--- tables.sql
+++ tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE Buyer
 CREATE TABLE Cart
     (
         Cart_id VARCHAR(7) NOT NULL,
-        PRIMARY KEY(Cart_id)
+        PRIMARY KEY(Cart_id),
         Brand_id VARCHAR(5) NOT NULL,
         FOREIGN KEY(Brand_id) REFERENCES Vehicle_brand(Brand_id)
        
@@ -22,7 +22,7 @@ CREATE TABLE Cart
 CREATE TABLE Vehicle_Brand
  (
         Brand_id VARCHAR(5) NOT NULL,
-        PRIMARY KEY(Brand_id)
+        PRIMARY KEY(Brand_id),
         Make_id VARCHAR(5) NOT NULL,
         FOREIGN KEY(Make_id) REFERENCES Make(Make_id)
  );
@@ -32,11 +32,39 @@ CREATE TABLE Make
         Make_id VARCHAR(5) NOT NULL,
         Model_Shape VARCHAR(3) NOT NULL,
         Engine_Power VARCHAR(20) NOT NULL,
-        0-60mph NUMBER(6) NOT NULL,
+        0-60mph NUMBER(3) NOT NULL,
         Price number(7) NOT NULL,
-        PRIMARY KEY (Make_id),
         Color_id VARCHAR(3) NOT NULL,
-        FOREIGN KEY(Cart_id) REFERENCES Cart(Cart_id)
         Accessories_id VARCHAR(3) NOT NULL,
+        PRIMARY KEY (Make_id),
+        FOREIGN KEY(Color_id) REFERENCES Color(Color_id),
         FOREIGN KEY(Accessories_id) REFERENCES Accessories(Accessories_id)
     ); 
+
+CREATE TABLE Payment
+    (
+        Payment_id VARCHAR(5) NOT NULL,
+        Payment_method VARCHAR(4) NOT NULL,
+        Customer_id VARCHAR(6) NOT NULL,
+        Cart_id VARCHAR(7) NOT NULL,
+        PRIMARY KEY (Payment_id),
+        FOREIGN KEY(Customer_id) REFERENCES Buyer(Customer_id),
+        FOREIGN KEY(Cart_id) REFERENCES Cart(Cart_id),
+        Total_amount numeric(6)
+    );
+    
+CREATE TABLE Accessories
+    (
+        Accessories_id VARCHAR(3) NOT NULL,
+        Accessory_price number(4) NOT NULL,
+        Accessory VARCHAR(10) NOT NULL,
+        PRIMARY KEY (Accessories_id)
+    );
+    
+Create Table Color
+    (
+        Color_id VARCHAR(3) NOT NULL,
+        Color_price number(3) NOT NULL,
+        Color VARCHAR(10) NOT NULL,
+        PRIMARY KEY (Color_id)
+    );
