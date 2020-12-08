5031896d9f12e2cd88b06a84c84e471edd3e2011
crowe4 <44951281+crowe4@users.noreply.github.com>
2020-12-08 14:25:31 -0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,71 +1,57 @@
-CREATE TABLE Buyer
-    (
-        Customer_id VARCHAR(6) NOT NULL,
-        Name VARCHAR(20) NOT NULL,
-        Address VARCHAR(20) NOT NULL,
-        Pincode NUMBER(6) NOT NULL,
-        Phone_number number(10) NOT NULL,
-        PRIMARY KEY (Customer_id),
-        Cart_id VARCHAR(7) NOT NULL,
-        FOREIGN KEY(Cart_id) REFERENCES Cart(Cart_id)
-    ); 
-    
-CREATE TABLE Cart
-    (
-        Cart_id VARCHAR(7) NOT NULL,
-        Purchased BOOL NOT NULL,
-        PRIMARY KEY(Cart_id),
-        Brand_id VARCHAR(5) NOT NULL,
-        FOREIGN KEY(Brand_id) REFERENCES Vehicle_brand(Brand_id)
-       
-    );
-    
-CREATE TABLE Vehicle_Brand
- (
-        Brand_id VARCHAR(5) NOT NULL,
-        PRIMARY KEY(Brand_id),
-        Make_id VARCHAR(5) NOT NULL,
-        FOREIGN KEY(Make_id) REFERENCES Make(Make_id)
- );
+USE `project`;
+DROP procedure IF EXISTS `color_price`;
 
-CREATE TABLE Make
-    (
-        Make_id VARCHAR(5) NOT NULL,
-        Model_Shape VARCHAR(3) NOT NULL,
-        Engine_Power VARCHAR(20) NOT NULL,
-        0-60mph NUMBER(3) NOT NULL,
-        Price number(7) NOT NULL,
-        Color_id VARCHAR(3) NOT NULL,
-        Accessories_id VARCHAR(3) NOT NULL,
-        PRIMARY KEY (Make_id),
-        FOREIGN KEY(Color_id) REFERENCES Color(Color_id),
-        FOREIGN KEY(Accessories_id) REFERENCES Accessories(Accessories_id)
-    ); 
+DELIMITER $$
+USE `project`$$
+CREATE PROCEDURE `color_price` (IN color varchar(50))
+BEGIN
+Select Color_price from Color Where Color_id = color;
+END$$
 
-CREATE TABLE Payment
-    (
-        Payment_id VARCHAR(5) NOT NULL,
-        Payment_method VARCHAR(4) NOT NULL,
-        Customer_id VARCHAR(6) NOT NULL,
-        Cart_id VARCHAR(7) NOT NULL,
-        PRIMARY KEY (Payment_id),
-        FOREIGN KEY(Customer_id) REFERENCES Buyer(Customer_id),
-        FOREIGN KEY(Cart_id) REFERENCES Cart(Cart_id),
-        Total_amount numeric(6)
-    );
-    
-CREATE TABLE Accessories
-    (
-        Accessories_id VARCHAR(3) NOT NULL,
-        Accessory_price number(4) NOT NULL,
-        Accessory VARCHAR(10) NOT NULL,
-        PRIMARY KEY (Accessories_id)
-    );
-    
-Create Table Color
-    (
-        Color_id VARCHAR(3) NOT NULL,
-        Color_price number(3) NOT NULL,
-        Color VARCHAR(10) NOT NULL,
-        PRIMARY KEY (Color_id)
-    );
+DELIMITER ;
+
+USE `project`;
+DROP procedure IF EXISTS `car_choose_price`;
+
+DELIMITER $$
+USE `project`$$
+CREATE PROCEDURE `car_choose_price` (IN mid varchar(6))
+BEGIN
+Select Price from Make Where Make_id = mid;
+
+END$$
+
+DELIMITER ;
+
+
+USE `project`;
+DROP procedure IF EXISTS `full_report`;
+
+DELIMITER $$
+USE `project`$$
+
+
+CREATE PROCEDURE `full_report` ( IN bid int,
+  OUT report_out varchar(500))
+BEGIN
+Declare cid, pri int default 0;
+Declare modelname varchar(30);
+Declare brandname varchar(10);
+Select Cart_id into cid
+From Buyer Join Cart 
+Where Buyer.Cart_id = Cart.Cart_id and Buyer.Customer_id = bid;
+
+Select Price into pri 
+From Buyer Join Cart
+Where Buyer.Cart_id = Cart.Cart_id and Buyer.Customer_id = bid;
+
+Select Brand_name into brandname
+From Buyer Natural join Cart Natural join Make natural join Vehicle_Brand
+Where Buyer.Customer_id = bid;
+
+Select Model_Shape into modelname
+From Buyer natural join Cart natural join Make
+Where Buyer.Customer_id = bid;
+
+Set report_out = CONCAT("Buyer id: " + str(bid) + " Cart id: " + str(cid) + " Price: $" + str(pri) + " Brand: " + brandname + " Model: " + modelname);
+END$$
